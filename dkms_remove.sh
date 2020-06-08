0776c8f894020ca72a559f881d26450f235bb2e7
Lin SiFuh <>
2020-06-08 23:11:39 +0800

    Simple bash script for uninstalling and removing dkms modules

---
+++ dkms_remove.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#BLACK=$(tput setaf 0)
+RED=$(tput setaf 1)
+#GREEN=$(tput setaf 2)
+YELLOW=$(tput setaf 3)
+#BLUE=$(tput setaf 4)
+#MAGENTA=$(tput setaf 5)
+#CYAN=$(tput setaf 6)
+#WHITE=$(tput setaf 7)
+
+BOLD=$(tput bold)
+RESET=$(tput sgr0)
+
+if [ "root" != "$USER" ]; then
+  echo "You are not root!"
+  exit 0
+fi
+
+dkms status | while read -r j ; do
+  DKMS=$(echo "$j" | sed -e 's\,\\g' | awk '{print "-m "$1" -v "$2" -k "$3 }')
+  read -r -p "Run the command: 'dkms ${YELLOW}${BOLD}uninstall${RESET} ${DKMS}' [y/N]? " PROMPT 0</dev/tty
+  if [[ ! ${PROMPT} =~ ^([yY][eE][sS]|[yY])$ ]] ; then
+    echo "Skipping..."
+  else
+    /usr/bin/dkms uninstall ${DKMS}
+    read -r -p "Run the command: 'dkms ${RED}${BOLD}remove${RESET} ${DKMS}' [y/N]? " PROMPT 0</dev/tty
+    if [[ ! ${PROMPT} =~ ^([yY][eE][sS]|[yY])$ ]] ; then
+      echo "Skipping..."
+    else
+      /usr/bin/dkms remove ${DKMS}
+    fi
+  fi
+done
